8349856f8f98a1de5fce2e9e624e9a0b3f04ec01
Enzo Do rosario <enzo.dorosario@redlab.io>
2022-03-24 23:32:07 +0100

    add ready text

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -12,6 +12,7 @@ export default {
     methods: {
         setPlayerReady() {
             this.socket.emit("playerReady", this.username);
+            this.isPlayerReady = true;
         },
     },
 };
@@ -20,23 +21,23 @@ export default {
     <p class="font-bold text-center text-xl my-8">Lobby</p>
 
     <div id="player-list" class="grid grid-cols-4 gap-4">
-        <div id="player-card" class="w-32">
-            <img class="player-pic" src="https://dummyimage.com/600x400" />
+        <div class="w-32">
+            <img src="https://dummyimage.com/600x400" />
             <p class="text-center">Virjil</p>
             <p class="text-center text-green-500">Ready</p>
         </div>
-        <div id="player-card" class="w-32">
-            <img class="player-pic" src="https://dummyimage.com/600x400" />
+        <div class="w-32">
+            <img src="https://dummyimage.com/600x400" />
             <p class="text-center">Tibo</p>
             <p class="text-center text-red-500">Pas ready</p>
         </div>
-        <div id="player-card" class="w-32">
-            <img class="player-pic" src="https://dummyimage.com/600x400" />
+        <div class="w-32">
+            <img src="https://dummyimage.com/600x400" />
             <p class="text-center">Charles</p>
             <p class="text-center text-red-500">Pas ready</p>
         </div>
-        <div id="player-card" class="w-32">
-            <img class="player-pic" src="https://dummyimage.com/600x400" />
+        <div class="w-32">
+            <img src="https://dummyimage.com/600x400" />
             <p class="text-center">Titou</p>
             <p class="text-center text-red-500">Pas ready</p>
         </div>
@@ -44,11 +45,14 @@ export default {
 
     <div class="mt-10 w-full text-center absolute bottom-10">
         <button
-            class="bg-purple-600 text-white py-3 text-md rounded w-1/3 hover:bg-purple-800"
+            class="bg-purple-600 text-white py-3 text-md rounded w-1/3 hover:bg-purple-800 disabled:bg-gray-300 disabled:text-slate-400"
             @click="setPlayerReady"
             :disabled="isPlayerReady"
         >
             Je suis ready
         </button>
+        <p class="text-slate-400 text-xs" v-if="isPlayerReady">
+            T'es bien ready bg
+        </p>
     </div>
 </template>
