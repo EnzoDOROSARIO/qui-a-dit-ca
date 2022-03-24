8d2687a49610f2f422bb8b9a185243d6f98c5438
Enzo Do rosario <enzo.dorosario@redlab.io>
2022-03-24 23:03:14 +0100

    update prettier command

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,3 @@
-<script lang="ts">
-export default {
-    data() {
-        return {
-            name: "",
-        };
-    },
-};
-</script>
 <template>
-    <div class="flex items-center justify-center h-screen">
-        <div>
-            <h1 class="font-sans font-bold uppercase text-4xl text-center">
-                Mets ton nom gros chien
-            </h1>
-            <input
-                type="text"
-                name="name"
-                class="w-full my-6 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                v-model="name"
-            />
-            <p v-if="name" class="text-xs text-slate-300">
-                ({{ name }} mdrrr ca pue sa mere)
-            </p>
-            <button
-                id="letsgo-button"
-                class="w-full bg-purple-600 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-purple-800 transition duration-300 disabled:bg-gray-300"
-                :disabled="!name"
-            >
-                LET'S GOOOO
-            </button>
-        </div>
-    </div>
+    <router-view />
 </template>
