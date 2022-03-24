fc8f50bf26c22ed8cfdbd25abb35f6bf65dd6d20
Enzo Do rosario <enzo.dorosario@redlab.io>
2022-03-24 22:40:47 +0100

    feat: add home page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
 </script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="flex items-center justify-center h-screen">
+    <div>
+      <h1 class="font-sans font-bold uppercase text-4xl text-center">
+        Mets ton nom gros chien
+      </h1>
+      <input
+        type="text"
+        name="name"
+        class="w-full my-6 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
+        v-model="name"
+      />
+      <p v-if="name" class="text-xs text-slate-300">
+        ({{ name }} mdrrr ca pue sa mere)
+      </p>
+      <button
+        id="letsgo-button"
+        class="w-full bg-purple-600 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-purple-800 transition duration-300 disabled:bg-gray-300"
+        :disabled="!name"
+      >
+        LET'S GOOOO
+      </button>
+    </div>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
